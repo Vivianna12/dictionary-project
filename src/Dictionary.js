93bb0577c20451cb6ca6bb02307c4d6c471e3c06
Vivianna12 <viviannasamite12@gmail.com>
2024-03-19 08:41:20 -0400

    working on the api element

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`searching for ${keyword} definition`);
+
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=sunset&key=f42atbc02aod4017c837cbfe1c70ef22`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
-    console.log(event.target.value);
     setKeyword(event.target.value);
   }
 
