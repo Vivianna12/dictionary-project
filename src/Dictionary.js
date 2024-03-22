c21e5529b915c4a20d45a96e1aad2ddaa4c5c2c5
Vivianna12 <viviannasamite12@gmail.com>
2024-03-22 19:45:17 -0400

    worked to get a new api to add photos based on the keyword

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
@@ -18,9 +20,18 @@ export default function Dictionary(props) {
     search();
   }
 
+  function handleSheCodeResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let shecodeApiKey = "f42atbc02aod4017c837cbfe1c70ef22";
+    let shecodeApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${shecodeApiKey}`;
+
+    axios.get(shecodeApiUrl).then(handleSheCodeResponse);
   }
 
   function handleKeywordChange(event) {
@@ -48,6 +59,7 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
